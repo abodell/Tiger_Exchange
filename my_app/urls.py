137bd7394093b046b142fbd1e86d6eb3e2ae3019
trinht017 <95663527+trinht017@users.noreply.github.com>
2023-04-02 17:46:55 -0400

    Feature/my listing (#6)
    * first
    
    * add validation form
    
    ---------
    
    Co-authored-by: Trinh Tran <trinht@Trinhs-MacBook-Air.local>

--- my_app/urls.py
+++ my_app/urls.py
@@ -5,7 +5,9 @@ app_name = 'my_app'
 
 urlpatterns = [
     path('', views.home,name='home'),
-    path('test_list_view', views.testListView.as_view(), name='list'),
-    path('test_listings/<int:pk>', views.TestDetailView.as_view(), name='detail_list'),
+    # path('test_list_view', views.testListView.as_view(), name='list'),
+    # path('test_listings/<int:pk>', views.TestDetailView.as_view(), name='detail_list'),
     path('create_listing/', views.createListingView, name='create_listing'),
+    path('my_listings/', views.myListingsView, name='my_listings'),
+    path('listings/<int:id>', views.listingDetailView, name='listing_detail')
 ]
