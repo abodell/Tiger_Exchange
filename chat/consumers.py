458431d53873b96c0c079701b1f7ae3c3ad4d780
Trinh Tran <trinht@Trinhs-MacBook-Air.local>
2023-04-14 00:57:56 -0400

    add functionality for messaging, need to style

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,36 +1,99 @@
-# chat/consumers.py
+from django.contrib.auth import get_user_model
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+from django.shortcuts import get_object_or_404
 import json
+from .models import Message, Chat, Contact
 
-from channels.generic.websocket import AsyncWebsocketConsumer
+User = get_user_model()
+
+def get_last_10_messages(chatId):
+    chat = get_object_or_404(Chat, id=chatId)
+    return chat.messages.order_by('timestamp').all()[:10]
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = "chat_%s" % self.room_name
+def get_user_contact(username):
+    user = get_object_or_404(User, username=username)
+    return get_object_or_404(Contact, user=user)
 
-        # Join room group
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
-        await self.accept()
+def get_current_chat(chatId):
+    return get_object_or_404(Chat, id=chatId)
 
-    async def disconnect(self, close_code):
-        # Leave room group
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+class ChatConsumer(WebsocketConsumer):
 
-    # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+    def fetch_messages(self, data):
+        messages = get_last_10_messages(data['chatId'])
+        content = {
+            'command': 'messages',
+            'messages': self.messages_to_json(messages)
+        }
+        self.send_message(content)
 
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "message": message}
+    def new_message(self, data):
+        user_contact = get_user_contact(data['from'])
+        message = Message.objects.create(
+            contact=user_contact,
+            content=data['message'])
+        current_chat = get_current_chat(data['chatId'])
+        current_chat.messages.add(message)
+        current_chat.save()
+        content = {
+            'command': 'new_message',
+            'message': self.message_to_json(message)
+        }
+        return self.send_chat_message(content)
+
+    def messages_to_json(self, messages):
+        result = []
+        for message in messages:
+            result.append(self.message_to_json(message))
+        return result
+
+    def message_to_json(self, message):
+        return {
+            'id': message.id,
+            'author': message.contact.user.username,
+            'content': message.content,
+            'timestamp': str(message.timestamp)
+        }
+
+    commands = {
+        'fetch_messages': fetch_messages,
+        'new_message': new_message
+    }
+
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        self.accept()
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
         )
 
-    # Receive message from room group
-    async def chat_message(self, event):
-        message = event["message"]
+    def receive(self, text_data):
+        data = json.loads(text_data)
+        self.commands[data['command']](self, data)
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+    def send_chat_message(self, message):
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    def send_message(self, message):
+        self.send(text_data=json.dumps(message))
+
+    def chat_message(self, event):
+        message = event['message']
+        self.send(text_data=json.dumps(message))
--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,44 @@
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
+import json
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden, Http404
+
+from .models import Contact, Chat
 
 # Create your views here.
 def index(request):
     return  render(request, 'chat/index.html')
 
+@login_required
 def room(request, room_name):
+
+    # Get the current user's contact object
+    current_user_contact, created = Contact.objects.get_or_create(user=request.user)
+
+    # Check if the chat room exists
+    try:
+        chat_room = Chat.objects.get(pk=room_name)
+    except Chat.DoesNotExist:
+        # If the chat room doesn't exist, return a 404 error
+        raise Http404("Chat room does not exist.")
+
+    # Check if the current user is a participant in the chat room
+    if not chat_room.participants.filter(pk=current_user_contact.pk).exists():
+        # If the user is not a participant, return a 403 Forbidden error
+        return HttpResponseForbidden("You are not a participant in this chat room.")
+    
+    # Get a list of chat rooms where the current user is a participant
+    user_chat_rooms = Chat.objects.filter(participants=current_user_contact)
+
+    chat_room_participants = [
+        (chat_room, chat_room.participants.all()) for chat_room in user_chat_rooms
+    ]
+
     context = {
-        'room_name': room_name
+        'room_name_json': mark_safe(json.dumps(room_name)),
+        'username': mark_safe(json.dumps(request.user.username)),
+        'user_chat_rooms': user_chat_rooms,
+        'chat_room_participants': chat_room_participants
     }
-    return render(request, 'chat/chatroom.html', context=context)
+    return render(request, 'chat/chat.html', context=context)
