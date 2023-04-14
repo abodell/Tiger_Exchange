b2e3f621fde164736509b7625c7508d10916395a
Trinh Tran <trinht@Trinhs-MacBook-Air.local>
2023-04-13 20:52:55 -0400

    hello

---
+++ chat/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return  render(request, 'chat/index.html')
+
+def room(request, room_name):
+    context = {
+        'room_name': room_name
+    }
+    return render(request, 'chat/chatroom.html', context=context)
